21e143ee7cb67346babc1886de33255ac7425944
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-08-24 06:45:23 +0545

    now this code works

--- js/script2.js
+++ js/script2.js
@@ -1,45 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const selectedProductDiv = document.getElementById('selected-product');
+  let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const selectedProductDiv = document.getElementById('selected-product');
-    let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-  
-    function updateCart(productName, productImage, productPrice) {
-      const existingItem = cartItems.find(item => item.name === productName);
-      
-      if (existingItem) {
-        alert("Item is already in the cart.");
-        return;
-      }
-      
-      cartItems.push({
-        name: productName,
-        image: productImage,
-        price: productPrice
-      });
-  
-      localStorage.setItem("cart", JSON.stringify(cartItems));
-      alert("Added to cart!");
+  function updateCart(productName, productImage, productPrice) {
+    const existingItem = cartItems.find(item => item.name === productName);
+    
+    if (existingItem) {
+      alert("Item is already in the cart.");
+      return;
     }
     
-    const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
-    addToCartButtons.forEach(button => {
-      button.addEventListener('click', function () {
-        const productDiv = this.closest('.row');
-        const productName = productDiv.querySelector('.card-text').textContent;
-        const productImage = productDiv.querySelector('.card-img-top').getAttribute('src');
-        const productPrice = productDiv.querySelector('.card-title').textContent;
-        updateCart(productName, productImage, productPrice);
-      });
+    cartItems.push({
+      name: productName,
+      image: productImage,
+      price: productPrice
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+    alert("Added to cart!");
+  }
+  
+  const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      const productDiv = this.closest('.card');
+      const productName = productDiv.querySelector('.card-title').textContent;
+      const productImage = productDiv.querySelector('.card-img-top').getAttribute('src');
+      const productPrice = productDiv.querySelector('.card-text').textContent;
+      
+      updateCart(productName, productImage, productPrice);
     });
   });
-  
-
-
-  
-
-
-   
-
-
-   
+});
 
