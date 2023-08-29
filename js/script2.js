4361c5f9c61fe2c12fcf79812de12668ccd9c977
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-08-29 13:22:35 +0545

    added productId but remove button doesnot work

--- js/script2.js
+++ js/script2.js
@@ -1,132 +1,12 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   const selectedProductDiv = document.getElementById('selected-product');
-//   let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-
-//   function updateCart(productName, productImage, productPrice) {
-//     const existingItem = cartItems.find(item => item.name === productName);
-    
-//     if (existingItem) {
-//       Swal.fire({
-//         icon: 'error',
-//         title: 'Oops...',
-//         text: 'Items is already in the cart',
-//         footer: '<a href="">Why do I have this issue?</a>'
-//       })
-//       return;
-//     }
-    
-//     cartItems.push({
-//       name: productName,
-//       image: productImage,
-//       price: productPrice
-//     });
-
-//     localStorage.setItem("cart", JSON.stringify(cartItems));
-//     // alert("Added to cart!");
-//     Swal.fire({
-//       position: 'top-end',
-//       icon: 'success',
-//       title: 'Items is added to cart',
-//       showConfirmButton: false,
-//       timer: 1000
-//     })
-//   }
-  
-//   const addToCartButtons = document.querySelectorAll('.add-to-cart-button');
-//   addToCartButtons.forEach(button => {
-//     button.addEventListener('click', function () {
-//       const productDiv = this.closest('.card');
-//       const productName = productDiv.querySelector('.card-text').textContent;
-//       const productImage = productDiv.querySelector('.card-img-top').getAttribute('src');
-//       const productPrice = productDiv.querySelector('.card-title').textContent;
-      
-//       updateCart(productName, productImage, productPrice);
-//     });
-//   });
-// });
-
-
-
-// // // Add to cart contents are under shopping-cart.html 
-
-// // // This  code is for payment form 
-
-
-// const paymentform_control = document.querySelector('form');
-// const main = document.querySelector('.main');
-// let indexx = 1; 
-// let submittedForm = [];
-// if(localStorage.getItem('submittedForm')){
-// submittedForm = JSON.parse(localStorage.getItem('submittedForm'));
-// }
-// Display();
-// paymentform_control.addEventListener("submit", function(event){
-// event.preventDefault();
-// const Firstname = document.getElementById('form6Example1').value;
-// const Lastname = document.getElementById('form6Example2').value;
-// const BankName = document.getElementById('form6Example3').value;
-// const AccountNumber = document.getElementById('form6Example4').value;
-// const PaymentGateway = document.querySelector('input[name="payment Gateway"]:checked').value;
-
-// const Data = {Firstname, Lastname, BankName, AccountNumber, PaymentGateway};
-// submittedForm.push(Data);
-// localStorage.setItem('submittedForm', JSON.stringify(submittedForm));
-// Display();
-// indexx ++;
-// });
-
-// // This is function to display the Data in main div
-// function Display() {
-//   main.innerHTML = "";
-//   // this is to create a table
-//   const table = document.createElement("table");
-//   table.className = "table align-middle mb-0 bg-white";
-//   table.innerHTML = `
-//     <thead class="bg-light">
-//       <tr>
-//         <th class="text-center">First Name</th>
-//         <th class="text-center">Last Name</th>
-//         <th class="text-center">Product Image</th>
-//         <th class="text-center">Purchased Price</th>
-//         <th class="text-center">Payment Gateway</th>
-//         <th class="text-center"></th>
-//       </tr>
-//     </thead>
-//     <tbody id="table-body"></tbody>
-//   `;
-//   main.appendChild(table);
-//   const tableBody = document.getElementById("table-body");
-//   submittedForm.forEach(formData => {
-//     const row = document.createElement("tr");
-//     row.innerHTML = `
-//       <td class="text-center">${formData.Firstname}</td>
-//       <td class="text-center">${formData.Lastname}</td>
-//       <td class="text-center">${formData.BankName}</td>
-//       <td class="text-center">${formData.AccountNumber}</td>
-//       <td class="text-center">${formData.PaymentGateway}</td>
-//       <td><span onclick='deleteSubmission()'>Remove</span></td>
-//     `;
-//     tableBody.appendChild(row);
-//   });
-// }
-
-// function deleteSubmission(index) {
-// submittedForm.splice(index, 1); 
-// localStorage.setItem("submittedForm", JSON.stringify(submittedForm));
-// main.innerHTML = "";
-// Display();
-// }
-
-
-
 
 document.addEventListener('DOMContentLoaded', function () {
   const selectedProductDiv = document.getElementById('selected-product');
   let cartItems = JSON.parse(localStorage.getItem('cart')) || [];
 
   function updateCart(productName, productImage, productPrice) {
+    // function updateCart(productName, productImage, productPrice, productId){
     const existingItem = cartItems.find(item => item.name === productName);
-    
+    // const existingItem = cartItems.findIndex(item => item.id === productId);
     if (existingItem) {
       Swal.fire({
         icon: 'error',
@@ -138,9 +18,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
     cartItems.push({
+      // id: productId,
       name: productName,
       image: productImage,
       price: productPrice
+
     });
 
     localStorage.setItem("cart", JSON.stringify(cartItems));
@@ -158,11 +40,12 @@ document.addEventListener('DOMContentLoaded', function () {
   addToCartButtons.forEach(button => {
     button.addEventListener('click', function () {
       const productDiv = this.closest('.card');
+      const productId = productDiv.querySelector('.card-text').textContent;
       const productName = productDiv.querySelector('.card-text').textContent;
       const productImage = productDiv.querySelector('.card-img-top').getAttribute('src');
       const productPrice = productDiv.querySelector('.card-title').textContent;
       
-      updateCart(productName, productImage, productPrice);
+      updateCart(productName, productImage, productPrice, productId);
     });
   });
 });
@@ -171,9 +54,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // Add to cart contents are under shopping-cart.html 
 
+
+
+
 // This  code is for payment form 
-
-
 const paymentform_control = document.querySelector('form');
 const main = document.querySelector('.main');
 let indexx = 1; 
@@ -238,3 +122,83 @@ localStorage.setItem("submittedForm", JSON.stringify(submittedForm));
 main.innerHTML = "";
 Display();
 }
+
+/* 
+// ...
+
+function updateCart(productName, productImage, productPrice, productId, isRemoveButton) {
+  const existingItemIndex = cartItems.findIndex(item => item.id === productId);
+
+  // ...
+
+  cartItems.push({
+    id: productId,        // Add the unique ID here
+    name: productName,
+    image: productImage,
+    price: productPrice,
+    quantity: selectedQuantity
+  });
+
+  localStorage.setItem("cart", JSON.stringify(cartItems));
+  renderCart();
+
+  // ...
+}
+
+// ...
+
+*/
+
+
+/*  document.addEventListener('DOMContentLoaded', function () {
+  const selectedProductDiv = document.getElementById('selected-product');
+  const totalDisplay = document.getElementById('totalPrice');
+
+  // Retrieve cart items from local storage
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+
+  function removeFromCart(productId) {
+    // Remove the item with the matching ID from the cart
+    const updatedCartItems = cartItems.filter(item => item.id !== productId);
+
+    // Update local storage and re-render cart
+    localStorage.setItem('cart', JSON.stringify(updatedCartItems));
+    renderCart(updatedCartItems);
+  }
+
+  function renderCart(items) {
+    selectedProductDiv.innerHTML = '';
+    
+    items.forEach(product => {
+      const productDiv = document.createElement('div');
+      productDiv.className = 'cart-item';
+      
+      // ... (create product elements similar to your existing code)
+      
+      // Create Remove button for each item
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove from Cart';
+      removeButton.className = 'remove-from-cart-button';
+      removeButton.dataset.productId = product.id;
+
+      removeButton.addEventListener('click', function () {
+        const productId = this.dataset.productId;
+        removeFromCart(productId);
+      });
+
+      productDiv.appendChild(removeButton);
+      selectedProductDiv.appendChild(productDiv);
+    });
+
+    // Calculate and display the total price
+    const totalPrice = items.reduce((total, product) => {
+      return total + (product.price * product.quantity);
+    }, 0);
+
+    totalDisplay.textContent = `Rs ${totalPrice.toFixed(2)} /-`;
+  }
+
+  // Initial render of the cart
+  renderCart(cartItems);
+});
+ */
