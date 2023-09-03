66da7347c15cee5b46e29fb2398ef0395a47e500
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-09-03 13:49:12 +0545

    this time applied the checkout data like email, phoneno cannot used twice

--- js/script2.js
+++ js/script2.js
@@ -179,7 +179,8 @@ if(CombinedStoredData) {
 
 
 // Now for checkOut.html
-function CheckOutContinue(){
+
+function CheckOutContinue() {
   const FirstName2 = document.getElementById("typeText").value;
   const LastName2 = document.getElementById('typeText2').value;
   const PhoneNO = document.getElementById('typePhone').value;
@@ -188,24 +189,37 @@ function CheckOutContinue(){
   const UserHouse = document.getElementById('typeText4').value;
   const PostalCode = document.getElementById('typeText5').value;
   const Zip = document.getElementById('typeText6').value;
-  const Checkbox = document.getElementById('flexCheckDefault').checked?document.getElementById('flexCheckDefault').value:'';
+  const Checkbox = document.getElementById('flexCheckDefault').checked ? document.getElementById('flexCheckDefault').value : '';
   const RadioButton = document.querySelector('input[name="flexRadioDefault"]:checked').value;
   const CitytoPick = document.querySelector('.form-select').value;
   const TextArea = document.getElementById('textAreaExample1').value;
 
-  let checkoutForm = {FirstName2, LastName2, PhoneNO, userEmail, userAddress, UserHouse, PostalCode, Zip, Checkbox, RadioButton, CitytoPick, TextArea};
-  // localStorage.setItem('checkOutData', JSON.stringify(checkoutForm));
-
-
+  let checkoutForm = { FirstName2, LastName2, PhoneNO, userEmail, userAddress, UserHouse, PostalCode, Zip, Checkbox, RadioButton, CitytoPick, TextArea };
 
   // Check if there is already data in local storage
   let checkoutData = JSON.parse(localStorage.getItem('checkOutData')) || [];
 
-  // Add the current set of data to the array
-  checkoutData.push(checkoutForm);
+  // Check if the new data is the same as any existing data
+  const isDataSame = checkoutData.some((data) => {
+    return (
+      data.FirstName2 === FirstName2 &&
+      data.LastName2 === LastName2 &&
+      data.PhoneNO === PhoneNO ||
+      data.userEmail === userEmail
+    );
+  });
 
-  // Store the updated array back in local storage
-  localStorage.setItem('checkOutData', JSON.stringify(checkoutData));
+  if (isDataSame) {
+    alert("Your Email or PhoneNo is already present");
+   
+  } else {
+    // Add the current set of data to the array
+    checkoutData.push(checkoutForm);
+
+    // Store the updated array back in local storage
+    localStorage.setItem('checkOutData', JSON.stringify(checkoutData));
+  }
 }
+
 
 
