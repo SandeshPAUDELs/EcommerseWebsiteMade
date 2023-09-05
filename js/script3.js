162b76eb84b3dcad59ef11ff3d4a01eb9e2606a5
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-09-05 21:26:27 +0545

    implemented add to cart logic in fetch api products page

--- js/script3.js
+++ js/script3.js
@@ -26,11 +26,13 @@ function displayProducts(data) {
                   <div class="card-body d-flex flex-column">
                       <h5 class="card-title">${product.title}</h5>
                       <p class="card-text">${product.category}</p>
+                      <p class="card-text">Product Id: ${product.id}</p>
                       <p>Rs${product.price.toFixed(2)}</p>
                   </div>
                   <div class="card-footer d-flex align-items-end pt-3 px-0 pb-0 mt-auto justify-content-between">
                       <a href="productDetails3.html" class="btn btn-primary">Details</a>
-                      <button class="btn btn-outline-primary w-100 add-to-cart-button" data-type="product">Add to cart</button>
+                      <button class="btn add-to-cart-button" data-type="product" data-product-id="${product.id}" onclick="addToCart(this)">Add to cart</button>
+
                   </div>
               </div>
           `;
@@ -62,3 +64,64 @@ fetchDataAndDisplay('https://dummyjson.com/products', displayProducts);
 // Fetch and display categories
 fetchDataAndDisplay('https://dummyjson.com/products/categories', displayCategories);
 
+
+// Now these are the code for add to cart logic
+const addedItems = [];
+function addToCart(button) {
+  const productId = button.getAttribute('data-product-id');
+  
+  // Check if the item has already been added
+  if (addedItems.includes(productId)) {
+    Swal.fire({
+      title: 'Item Already in Cart',
+      text: 'This item is already in your cart.',
+      icon: 'info'
+    });
+    return;
+  }
+  // local storage ma data set garna ko lagi
+  const apicart = JSON.parse(localStorage.getItem('apicart')) || [];
+  const existingProduct = apicart.find(item => item.productId === productId);
+
+  if (existingProduct) {
+    existingProduct.quantity++;
+  } else {
+    apicart.push({ productId, quantity: 1 });
+  }
+
+  localStorage.setItem('apicart', JSON.stringify(apicart));
+  // Add the item to the list of added items
+  addedItems.push(productId);
+  Swal.fire({
+    title: 'Item Added to Cart',
+    text: 'The item has been added to your cart.',
+    icon: 'success'
+  });
+
+  displayCart(); // Update the cart display
+}
+
+// Function to display cart contents in another HTML file
+function displayCart() {
+  const apicart = JSON.parse(localStorage.getItem('apicart')) || [];
+  const cartDisplay = document.getElementById('cart-display');
+
+  if (cartDisplay) {
+    cartDisplay.innerHTML = ''; // Clear previous content
+
+    if (apicart.length === 0) {
+      cartDisplay.textContent = 'Your cart is empty.';
+    } else {
+      apicart.forEach(item => {
+        const product = getProductById(item.productId);
+        if (product) {
+          const cartItem = document.createElement('div');
+          cartItem.textContent = `${product.title} (Quantity: ${item.quantity})`;
+          cartDisplay.appendChild(cartItem);
+        }
+      });
+    }
+  }
+}
+
+displayCart();
