910f56f1388fc865d5fd2a20e282d9a03698b8c5
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-09-12 08:50:36 +0545

    this time login is success if user writes the same username and password present in api

--- js/login.js
+++ js/login.js
@@ -4,23 +4,39 @@
   const loginPasswordInput = document.querySelector('#loginPassword');
   
 
+//   loginButton.addEventListener('click', (e) => {
+//     fetch('https://dummyjson.com/auth/login', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+    
+//     username: loginUsername.value,
+//     password: loginPasswordInput.value,
+//     // expiresInMins: 60, // optional
+//   })
+// })
+// .then(res => res.json())
+// .then(console.log);
+//   })
+  
 
   // Function to handle login  
   loginButton.addEventListener('click', (e) => {
-    // console.log(loginUsername, loginPasswordInput)
     const login = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        userName: loginUsername.value,
+        username: loginUsername.value,
         password: loginPasswordInput.value,
       }),
     };
     fetch('https://dummyjson.com/auth/login', login)
     .then(res => res.json())
     .then(console.log);
+
+
   });
 
   
