a5af8f4566fc2722f046e416ab0315da55b652d3
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-09-13 11:59:44 +0545

    removed unnecessary files and make it

--- js/carts.js
+++ js/carts.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   
     const userId = userData.id;
-  
+    
     fetch(`https://dummyjson.com/users/${userId}/carts`)
       .then(res => {
         if (res.ok) {
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const CartInfoContainer = document.getElementById('Cart-Items');
         const mainContainer = document.createElement('div');
         mainContainer.classList.add('main-container');
+        
   
         const userTable = document.createElement('table');
         userTable.classList.add('cart-info');
@@ -67,28 +68,27 @@ document.addEventListener('DOMContentLoaded', () => {
   
         userTable.appendChild(tableHeader);
         userTable.appendChild(cartItemsContainer);
-  
+        tableHeader.appendChild(headerRow);
+
         const purchasePriceSection = document.createElement('div');
         purchasePriceSection.classList.add('PurchasePrice');
-  
         purchasePriceSection.innerHTML = `
           <div class="card shadow-0 border">
             <div class="card-body">
               <div class="d-flex justify-content-between">
-                <p class="mb-2">Discount:</p>
+                <p class="mb-2">Total Price</p>
                 <p class="mb-2 text-success">Rs ${cartData.carts[0].total}</p>
               </div>
               <div class="d-flex justify-content-between">
-                <p class="mb-2">TAX:</p>
-                <p class="mb-2">Rs 000</p>
-              </div>
+              <p class="mb-2">Discount:</p>
+            </div>
               <div class="d-flex justify-content-between">
-                <p class="mb-2">Total price:</p>
+                <p class="mb-2">Price After Discount:</p>
                 <p class="mb-2" id="totalPrice">Rs ${cartData.carts[0].discountedTotal}</p>
               </div>
               <div class="mt-3">
                 <a href="payment.html" class="btn btn-success w-100 shadow-0 mb-2"> Make Purchase </a>
-                <a href="checkout.html" class="btn btn-light w-100 border mt-2">Check Out</a>
+                <a href="checkout.html" class="btn btn-light w-100 border mb-2">Check Out</a>
               </div>
             </div>
           </div>
@@ -110,111 +110,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
-
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-// //   fetch('https://dummyjson.com/carts/user/15')
-// fetch('https://dummyjson.com/users/15/carts')
-
-//       .then(res => {
-//           if (res.ok) {
-//               return res.json();
-//           } else {
-//               throw new Error('Failed to fetch cart items');
-//           }
-//       })
-//       .then(cartData => {
-//           const CartInfoContainer = document.getElementById('Cart-Items');
-//           const mainContainer = document.createElement('div');
-//           mainContainer.classList.add('main-container');
-
-          
-//           const userTable = document.createElement('table');
-//           userTable.classList.add('cart-info');
-
-//           const tableHeader = document.createElement('thead');
-//           const headerRow = document.createElement('tr');
-
-//           const columns = ['Product', 'Price', 'Quantity', 'Total'];
-
-//           columns.forEach(columnName => {
-//               const th = document.createElement('th');
-//               th.textContent = columnName;
-//               headerRow.appendChild(th);
-//           });
-
-//           tableHeader.appendChild(headerRow);
-
-//           const cartItemsContainer = document.createElement('tbody');
-//           cartItemsContainer.classList.add('cart-items');
-
-//           cartData.carts[0].products.forEach(product => {
-//               const productRow = document.createElement('tr');
-//               productRow.classList.add('product');
-
-//               const productTitle = document.createElement('td');
-//               productTitle.textContent = product.title;
-
-//               const productPrice = document.createElement('td');
-//               productPrice.textContent = `Rs ${product.price.toFixed(2)} /-`;
-
-//               const productQuantity = document.createElement('td');
-//               productQuantity.textContent = product.quantity;
-
-//               const productTotal = document.createElement('td');
-//               const totalPrice = (product.price * product.quantity).toFixed(2);
-//               productTotal.textContent = `Rs ${totalPrice} /-`;
-//               // need to calculate total price of all items in cart
-
-//               productRow.appendChild(productTitle);
-//               productRow.appendChild(productPrice);
-//               productRow.appendChild(productQuantity);
-//               productRow.appendChild(productTotal);
-
-//               cartItemsContainer.appendChild(productRow);
-//           });
-
-//           userTable.appendChild(tableHeader);
-//           userTable.appendChild(cartItemsContainer);
-
-
-//           const purchasePriceSection = document.createElement('div');
-//           purchasePriceSection.classList.add('PurchasePrice');
-
-//           purchasePriceSection.innerHTML = `
-//                 <div class="card shadow-0 border">
-//                   <div class="card-body">
-//                       <div class="d-flex justify-content-between">
-//                           <p class="mb-2">Discount:</p>
-//                           <p class="mb-2 text-success">Rs ${cartData.carts[0].total}</p>
-//                       </div>
-//                       <div class="d-flex justify-content-between">
-//                           <p class="mb-2">TAX:</p>
-//                           <p class="mb-2">Rs 000</p>
-//                       </div>
-//                       <div class="d-flex justify-content-between">
-//                           <p class="mb-2">Total price:</p>
-//                           <p class="mb-2" id="totalPrice">Rs ${cartData.carts[0].discountedTotal}</p>
-//                       </div>
-//                       <div class="mt-3">
-//                           <a href="payment.html" class="btn btn-success w-100 shadow-0 mb-2"> Make Purchase </a>
-//                           <a href="checkout.html" class="btn btn-light w-100 border mt-2">Check Out</a>
-//                       </div>
-//                   </div>
-//               </div>
-          
-//           `;
-
-//           mainContainer.appendChild(userTable);
-//           mainContainer.appendChild(purchasePriceSection);
-
-//           CartInfoContainer.appendChild(mainContainer);
-
-//           console.log(cartData);
-//       })
-//       .catch(error => {
-//           console.error(error);
-//       });
-// });
---
+++ js/orderDetails.js
@@ -0,0 +1,62 @@
+const storedCheckoutData = JSON.parse(localStorage.getItem('checkOutData'));
+
+if (storedCheckoutData) {
+  let tableHtml = `
+    <table>
+      <tr>
+        <th>FirstName</th>
+        <th>lastName</th>
+        <th>PhoneNo.</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Delivery Status</th>
+      </tr>
+  `;
+
+  for (const data of storedCheckoutData) {
+    tableHtml += `
+      <tr>
+        <td class = "text-center">${data.FirstName2}</td>
+        <td class = "text-center">${data.LastName2}</td>
+        <td class = "text-center">${data.PhoneNO}</td>
+        <td class = "text-center">${data.userEmail}</td>
+        <td class = "text-center">${data.userAddress}</td>
+  <td>
+    <select name="Status" id="yourSelectElement">
+      <option style="color: red;" value="1">Pending</option>
+      <option value="2">Awating payment</option>
+      <option value="3">Awating Shipping</option>
+      <option value="4">Awating Fulfilment</option>
+      <option value="5">Cancelled</option>
+      <option value="6">Declined</option>
+      <option value="7" disabled>Refunded</option>
+      <option value="8">Manual Verification Required</option>
+      <option value="9">Partially Refunded</option>
+    </select>
+  </td>
+      </tr>
+    `;
+ 
+  }
+
+  document.getElementById('displayCheckout').innerHTML = tableHtml;
+  tableHtml += `</table>`;
+
+} 
+
+
+// Now this is code for Storing the select option data in local storage
+
+document.addEventListener('DOMContentLoaded', function () {
+  const selectElement = document.getElementById('yourSelectElement');
+
+  if (selectElement) {
+    selectElement.addEventListener('change', function () {
+      const selectedOption = selectElement.options[selectElement.selectedIndex];
+      const selectedOptionText = selectedOption.text;
+      console.log(selectedOptionText)
+
+      localStorage.setItem('selectedOptionText', selectedOptionText);
+    });
+  }
+});
