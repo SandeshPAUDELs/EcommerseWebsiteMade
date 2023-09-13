42ad9d21ca658615877eb02c0b23b0c25a611651
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-09-13 21:32:48 +0545

    users data is created with datatables.net

--- js/userTable.js
+++ js/userTable.js
@@ -1,136 +1,60 @@
-fetch('https://dummyjson.com/users')
-  .then((res) => res.json())
-  .then((data) => {
-    const UsersAllData = document.getElementById('user-details-table');
-    const users = data.users;
+function fetchAndDisplayUserDetails(userId) {
+  fetch(`https://dummyjson.com/auth/users/${userId}`)
+      .then(res => res.json())
+      .then(userDetails => {
+          const userDetailsHTML = `
+              <p><strong>ID:</strong> ${userDetails.id}</p>
+              <p><strong>First Name:</strong> ${userDetails.firstName}</p>
+              <p><strong>Last Name:</strong> ${userDetails.lastName}</p>
+              <p><strong>Maiden Name:</strong> ${userDetails.maidenName}</p>
+              <p><strong>Age:</strong> ${userDetails.age}</p>
+              <p><strong>Gender:</strong> ${userDetails.gender}</p>
+              <p><strong>Email:</strong> ${userDetails.email}</p>
+              <p><strong>Phone:</strong> ${userDetails.phone}</p>
+              <p><strong>Username:</strong> ${userDetails.username}</p>
+              <p><strong>Password:</strong> ${userDetails.password}</p>
+              <p><strong>Birth Date:</strong> ${userDetails.birthDate}</p>
 
-    if (Array.isArray(users)) {
-      const table = document.createElement('table');
-      const thead = document.createElement('thead');
-      const headerRow = document.createElement('tr');
-      const headers = ['Image', 'Name', 'Email', 'Contact NO', 'Gender', 'Age', 'Username', 'passcode'];
+          `;
 
-      headers.forEach((headerText) => {
-        const th = document.createElement('th');
-        th.textContent = headerText;
-        headerRow.appendChild(th);
+          document.getElementById('userDetailsBody').innerHTML = userDetailsHTML;
       });
+}
 
-      thead.appendChild(headerRow);
-      const tbody = document.createElement('tbody');
+function openUserDetailsModal(userId) {
+  fetchAndDisplayUserDetails(userId);
+  $('#userDetailsModal').modal('show');
+}
+function fetchDataAndInitializeDataTable() {
+      fetch('https://dummyjson.com/auth/users', {
+              method: 'GET', 
+              headers: {
+                  'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTUsInVzZXJuYW1lIjoia21pbmNoZWxsZSIsImVtYWlsIjoia21pbmNoZWxsZUBxcS5jb20iLCJmaXJzdE5hbWUiOiJKZWFubmUiLCJsYXN0TmFtZSI6IkhhbHZvcnNvbiIsImdlbmRlciI6ImZlbWFsZSIsImltYWdlIjoiaHR0cHM6Ly9yb2JvaGFzaC5vcmcvYXV0cXVpYXV0LnBuZyIsImlhdCI6MTY5NDYxODU3NSwiZXhwIjoxNjk0NjIyMTc1fQ.V4m7bGqAKrQ3SMyySzbNn0CHXfSruzPWQ-cNGVkkcAA', 
+                  'Content-Type': 'application/json'
+              },
+              })
+          .then(res => res.json())
+          .then(data => {
+              const users = data.users;
 
-      users.forEach((user) => {
-        const row = document.createElement('tr');
+              const dataTable = $('#userTable').DataTable({
+                  data: users,
+                  columns: [
+                      { data: 'id' },
+                      { data: 'firstName' },
+                      { data: 'lastName' },
+                      { data: 'email' },
+                      { 
+                          data: 'id',
+                          render: function(data, type, row) {
+                              return `<button class="btn btn-primary" onclick="openUserDetailsModal(${data})">View</button>`;
+                          },
+                      },
+                  ]
+              });
+          });
+  }
 
-        const idCell = document.createElement('td');
-        idCell.textContent = `${user.id}`;
-
-        const imageCell = document.createElement('td');
-        imageCell.innerHTML = `<img src="${user.image}" width="50" height="50" />`;
-
-        const nameCell = document.createElement('td');
-        nameCell.textContent = `${user.firstName} ${user.maidenName} ${user.lastName}`;
-
-        const emailCell = document.createElement('td');
-        emailCell.textContent = user.email;
-
-        const phoneCell = document.createElement('td');
-        phoneCell.textContent = user.phone;
-
-        const genderCell = document.createElement('td');
-        genderCell.textContent = user.gender;
-
-        const ageCell = document.createElement('td');
-        ageCell.textContent = user.age;
-        
-        const usernameCell = document.createElement('td');
-        usernameCell.textContent = user.username;
-
-        const passcodeCell = document.createElement('td');
-        passcodeCell.textContent = user.password;
-
-        
-        row.appendChild(imageCell);
-        row.appendChild(nameCell);
-        row.appendChild(emailCell);
-        row.appendChild(phoneCell);
-        row.appendChild(genderCell);
-        row.appendChild(ageCell);
-        row.appendChild(usernameCell);
-        row.appendChild(passcodeCell);
-
-
-        tbody.appendChild(row);
-      });
-
-      table.appendChild(thead);
-      table.appendChild(tbody);
-
-      UsersAllData.appendChild(table);
-    }
+$(document).ready(function () {
+      fetchDataAndInitializeDataTable();
   });
-
-
-// import { fetchDataAndDisplay, getUrlParameter } from './utils.js';
-
-// function displayUsers(data) {
-//   const userDetailsTable = document.getElementById('user-details-table');
-//   const users = data.users;
-
-//   if (Array.isArray(users)) {
-//     const tbody = document.createElement('tbody');
-
-//     users.forEach((user) => {
-//       const row = document.createElement('tr');
-
-//       const imageCell = document.createElement('td');
-//       imageCell.innerHTML = `<img src="${user.image}" width="50" height="50" />`;
-
-//       const nameCell = document.createElement('td');
-//       nameCell.textContent = `${user.firstName} ${user.maidenName} ${user.lastName}`;
-
-//       const emailCell = document.createElement('td');
-//       emailCell.textContent = user.email;
-
-//       const phoneCell = document.createElement('td');
-//       phoneCell.textContent = user.phone;
-
-//       const genderCell = document.createElement('td');
-//       genderCell.textContent = user.gender;
-
-//       const ageCell = document.createElement('td');
-//       ageCell.textContent = user.age;
-
-//       const usernameCell = document.createElement('td');
-//       usernameCell.textContent = user.username;
-
-//       const passcodeCell = document.createElement('td');
-//       passcodeCell.textContent = user.password;
-
-//       row.appendChild(imageCell);
-//       row.appendChild(nameCell);
-//       row.appendChild(emailCell);
-//       row.appendChild(phoneCell);
-//       row.appendChild(genderCell);
-//       row.appendChild(ageCell);
-//       row.appendChild(usernameCell);
-//       row.appendChild(passcodeCell);
-
-//       tbody.appendChild(row);
-//     });
-
-//     userDetailsTable.appendChild(tbody);
-//   }
-// }
-
-// $(document).ready(function () {
-//   const search = getUrlParameter('search');
-
-//   if (search !== null) {
-//     fetchDataAndDisplay('https://dummyjson.com/users/search?q='+search, displayUsers);
-//     document.getElementById('search').value = search;
-//   }
-//   if(search !== null) {
-//     fetchDataAndDisplay('https://dummyjson.com/users', displayUsers);
-//   }
-// });
